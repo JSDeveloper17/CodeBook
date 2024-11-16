ea2dd4c9af6019e3157b2d0dadac194966c98f29
JSDeveloper17 <securemebro@gmail.com>
2024-11-16 08:31:57 +0530

    CodeBook Deployment

---
+++ src/services/index.js
@@ -0,0 +1,3 @@
+export {login, register,logout} from "./authService"
+export {getUser, getUserOrder, createOrder }from "./dataservice"
+export { getProduct, getProductList, getFeaturedList} from "./productService"
